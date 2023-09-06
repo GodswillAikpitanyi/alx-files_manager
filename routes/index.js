d0391ae369970c67c4fbae0aa18f0694566a08a4
Almadny <Almadny2u@gmail.com>
2023-09-06 22:18:39 +0000

    feature: Added Appcontroller, Server and Index.js

---
+++ routes/index.js
@@ -0,0 +1,5 @@
+import AppController from ('../controllers/AppController');
+import app from ('../server');
+
+app.get('/status', await AppController.getStatus);
+app.get('/stats', await AppController.getStats);
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+import { route } from ('./routes/index.js')
+default export const app = express()
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
